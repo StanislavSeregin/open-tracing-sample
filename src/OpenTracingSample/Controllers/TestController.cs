747cba9ccbaa85cd744687327452f3908629b2ab
stanislav <stas_seregin@mail.ru>
2020-09-06 16:03:44 +0300

    Initial commit - create empty app

---
+++ src/OpenTracingSample/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OpenTracingSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogWarning("This is test warning!");
+            _logger.LogError("This is test error!");
+            _logger.LogCritical("This is test critical!");
+            return Ok("Test");
+        }
+    }
+}
